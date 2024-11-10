609a12807e9f3c6380a9e4d6e46ad4782e660633
NirojBhattarai <neerajbhttri@gmail.com>
2024-11-10 16:03:07 +0545

    Update Social Media Icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
-    <footer className="text-center text-white bg-slate-900">
+    <footer className="text-center text-white">
       {/* Social Network Section */}
-      <section className="bg-blue-600 p-4 border-b border-gray-700 flex justify-center lg:justify-between">
+      <section className="bg-blue-600 p-4 border-b flex justify-center lg:justify-center space-x-4">
         <div className="hidden lg:block text-white">
           <span>Get connected with us on social networks:</span>
         </div>
 
-        <div className="text-white flex space-x-4">
+        <div className="text-white flex space-x-4 pt-1">
           <a href="/" className="hover:text-gray-400">
-            <i className="bi bi-facebook"></i>
+            <i>
+              <FaLinkedin />{" "}
+            </i>
           </a>
           <a href="/" className="hover:text-gray-400">
-            <i className="bi bi-twitter"></i>
-          </a>
-          <a href="/" className="hover:text-gray-400">
-            <i className="bi bi-google"></i>
-          </a>
-          <a href="/" className="hover:text-gray-400">
-            <i className="bi bi-instagram"></i>
-          </a>
-          <a href="/" className="hover:text-gray-400">
-            <i className="bi bi-linkedin"></i>
-          </a>
-          <a href="/" className="hover:text-gray-400">
-            <i className="bi bi-github"></i>
+            <i>
+              <FaGithub />{" "}
+            </i>
           </a>
         </div>
       </section>
