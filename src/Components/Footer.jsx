8639197b42364a82b404c7089eb8dc7863e5e396
NirojBhattarai <neerajbhttri@gmail.com>
2024-11-11 19:57:16 +0545

    Update Footer Component, Social Media Links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer className="text-center text-white">
-      {/* Social Network Section */}
-      <section className="bg-blue-600 p-4 border-b flex justify-center lg:justify-center space-x-4">
-        <div className="hidden lg:block text-white">
+    <footer className="text-center text-white bg-gradient-to-r from-blue-800 to-purple-800 py-6">
+      {/* Social Links Section */}
+      <section className="flex flex-col items-center lg:flex-row lg:justify-between px-6 lg:px-16 space-y-4 lg:space-y-0">
+        <div className="text-white hidden lg:block lg:mr-4">
           <span>Get connected with us on social networks:</span>
         </div>
 
-        <div className="text-white flex space-x-4 pt-1">
-          <a href="/" className="hover:text-gray-400">
-            <i>
-              <FaLinkedin />{" "}
-            </i>
+        <div className="flex space-x-6 text-2xl">
+          <a
+            href="https://www.linkedin.com/in/nirojbhattarai/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-400 transition duration-300"
+          >
+            <FaLinkedin />
           </a>
-          <a href="/" className="hover:text-gray-400">
-            <i>
-              <FaGithub />{" "}
-            </i>
+          <a
+            href="https://github.com/NirojBhattarai"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-400 transition duration-300"
+          >
+            <FaGithub />
           </a>
         </div>
       </section>
 
       {/* Copyright Section */}
-      <div className="bg-blue-600 text-white text-center p-4">
-        © 2024 Copyright:
-        <a
-          className="text-white font-bold hover:underline"
-          href="https://nirojbhattarai.com.np/"
-        >
-          nirojbhattarai.com.np
-        </a>
+      <div className="bg-gradient-to-r from-blue-800 to-purple-800 w-full text-white text-center py-2 mt-4">
+        <p className="text-sm">
+          &copy; 2024
+          <a
+            href="https://nirojbhattarai.com.np/"
+            className="text-white font-bold hover:underline ml-1"
+          >
+            nirojbhattarai.com.np
+          </a>
+        </p>
       </div>
     </footer>
   );
